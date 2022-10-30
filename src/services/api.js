62b6d11a622a4c8598873a42c22ab511f0567650
Abner <abnerodrigs@gmail.com>
2022-10-30 18:14:10 -0300

    ajusta request

--- src/services/api.js
+++ src/services/api.js
@@ -4,16 +4,7 @@ const api = axios.create({
     baseURL: "https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br"
 });
 
-api.interceptors.request.use(async config => {
-    // config.headers['Access-Control-Allow-Origin'] = '*';
-    // config.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,PATCH,OPTIONS';
-    // config.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
-    //delete cache
-    // config.headers['Cache-Control'] = 'no-cache';
-    // config.headers['Pragma'] = 'no-cache';
-    // config.headers['Expires'] = '0';
-
-    return config;
-});
+axios.defaults.withCredentials = false;
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
 export default api;
