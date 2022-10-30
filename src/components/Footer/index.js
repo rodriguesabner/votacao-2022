0a3dbdb0b8d50ae20527e0c0a7d8ae11ce35b12a
Abner <abnerodrigs@gmail.com>
2022-10-30 15:27:57 -0300

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Layout} from "./styles";
+
+const Header = () => {
+    return (
+        <Layout>
+
+        </Layout>
+    );
+};
+
+export default Header;
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+  width: 100%;
+  height: 68px;
+  background-color: #3c3c94;
+`;
